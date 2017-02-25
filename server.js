454ab5870f461cca60c319c0e0828758be7f471f
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-02-25 23:44:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res){
     //articleName==article-name
+    var articleName= req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 

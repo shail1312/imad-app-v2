8e5b4425541b95717b991ad3406d79650ecec869
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-02-25 23:52:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles: {
+var articles= {
     'article-one': {
         title: 'Article one | Shailesh',
         heading: 'Article one',

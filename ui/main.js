7b1ec1be5cd8d69604acbd0b6c6dacc80c43977a
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-03-01 21:50:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,25 @@
 //counter code
 var button = document.getElementById('counter');
-var counter =0;
 button.onClick = function(){
     
-    //make request to counter exd point
+    //create the request
+    var request = new XMLHttpRequest();
     
     //capture the responce and store it in a variable
-    
-    //rendre the variable in the correct spam
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+            
+        }
+        //Not done yet
+        
+    };
+    //Make the request
+    request.open('GET','http://shail1312.imad.hasura-app.io/counter',true)
+    request.send(null);
 };

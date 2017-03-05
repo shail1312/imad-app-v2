58dfce16c444e5e928d0f43546cf2f40a96c9860
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-03-05 19:42:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,16 +23,16 @@ button.onclick = function(){
     request.send(null);
 };
 
-//submit name 
+//submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0; i<name.length; i++){
-        list += '<li>' +names[i] +'</li>'; 
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+  var names = ['name1','name2','name3'];
+  var list = '';
+  for(var i=0; i<names.length; i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ui.innerHTML = list;
 };

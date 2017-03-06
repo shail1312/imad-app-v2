a044ad81edc8723fdc1e1a4ac62a55abac6111b6
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-03-06 22:21:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,13 +28,33 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-   var names = ['name1','name2','name3','name4'];
-    var list = '';
-    var i = 0;
-    for(i=0; i<4; i++){
-        alert(i);
-        list = list + '<li>' +names[i] +'</li>'; 
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
+    //create the request
+    var request = new XMLHttpRequest();
+    
+    //capture the responce and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status == 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                //var i = 0;
+                for(var i=0; i<names.length; i++){
+                    alert(i);
+                    list = list + '<li>' +names[i] +'</li>'; 
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        //Not done yet
+        
+    };
+    //Make the request
+    request.open('GET','http://shail1312.imad.hasura-app.io/submit-name?name='+name,true)
+    request.send(null);
+    
+   
 };

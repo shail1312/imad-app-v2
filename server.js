c397321df19404fe8edc4675f08478aaf0fec976
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-03-11 19:34:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,15 +113,12 @@ app.get('/articles/:articleName',function(req,res){
     var articleName= req.params.articleName;
     
     pool.query("SELECT * FROM article_tab where title = '"+req.params.articleName+"' ",function(err,result){
-        alert('found');
-         if(err){
+        if(err){
            res.status(500).send(err.toString());
        } else{
-           alert('found');
           if(result.rows.length==0){
               res.status(404).send('Article not found');
           } else {
-              alert('found');
               var articleData = result.rows[0];
               res.send(createTemplate(articles[articleData]));
           }

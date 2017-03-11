e40968e756411d21ea2a9ed4adb4cd0668e2506b
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-03-11 19:13:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,6 +120,7 @@ app.get('/articles/:articleName',function(req,res){
               res.status(404).send('Article not found');
           } else {
               var articleData = result.rows[0];
+              alert('found');
               res.send(createTemplate(articles[articleData]));
           }
        }

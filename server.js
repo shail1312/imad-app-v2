09d2c4c48ed5c64f09c89859a03f29cc88f61194
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-03-12 18:21:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,13 +56,12 @@ app.get('/', function (req, res) {
 });
 
 function hash(input, salt){
-    //How do create hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
-app.get('/hash/:input', function(req, res){
-    var hasedString = hash(req.params.input, 'this-is-some-random-string');
+app.get('/hash/:input',function(req, res){
+    var hashedString = hash(req.params.input,'this-is-same-random-string');
     res.send(hashedString);
 });
 

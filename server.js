d5582112052d9ba3e1aa4e19f86827cf46bc80d7
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-03-12 09:22:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,36 +14,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles= {
-    'article-one': {
-        title: 'Article one | Shailesh',
-        heading: 'Article one',
-        date: 'Feb26-2016',
-        content:` 
-                <p>This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. 
-                </p>
-                <p>This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. 
-                </p>
-                <p>This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. This is content of my first article. 
-                </p>`
-    },
-    'article-two': {
-        title: 'Article two | Shailesh',
-        heading: 'Article two',
-        date: 'March-2016',
-        content: ` 
-                <p>This is content of my second article. </p>`
-    },
-    'article-three': {
-        title: 'Article three | Shailesh',
-        heading: 'Article three',
-        date: 'April-2016',
-        content: ` 
-                <p>This is content of my third article.</p>`
-    }
-};
-
-
 function createTemplate(data){
     var title=data.title;
     var heading=data.heading;

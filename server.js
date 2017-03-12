8db82980710e33b5cd5e7d1ea128496258a9504e
Shailesh Kumar Pandey <shail1312@hotmail.com>
2017-03-12 09:20:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/submit-name', function (req, res) {
 });
 
 app.get('/articles/:articleName',function(req,res){
-    pool.query("Select * from article_tab where title = '" + req.params.articleName + "'",function(err,result){
+    pool.query("Select * from article_tab where title = $1",[req.params.articleName],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else {
